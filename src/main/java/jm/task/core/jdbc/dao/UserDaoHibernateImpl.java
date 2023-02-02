a8313d8589c9c3bf1e52c47ceab1164918dfdc2e
Bart22422 <84784707+Bart22422@users.noreply.github.com>
2023-02-03 00:43:45 +0300

    create connect whithout try whith resourses

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,17 +2,21 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import net.bytebuddy.asm.Advice;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
     private static final String SQL_TABLE = "CREATE TABLE IF NOT EXISTS `instructor`.`usersTable` " +
             " (`id` INT NOT NULL AUTO_INCREMENT," +
             "`name` VARCHAR(45) NOT NULL," +
@@ -21,8 +25,8 @@ public class UserDaoHibernateImpl implements UserDao {
             " PRIMARY KEY (`id`)) " +
             "ENGINE = InnoDB " +
             "DEFAULT CHARACTER SET = utf8";
-    private static final String dropTable = "DROP TABLE";
-    private static final String cleanTAble = "DELETE FROM USERS\n" +
+    private static final String dropTable = "DROP TABLE usersTable";
+    private static final String cleanTAble = "DELETE FROM usersTable\n" +
             "WHERE ID > 0";
 
     public UserDaoHibernateImpl() {
@@ -32,46 +36,32 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        Transaction transaction = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            session.createNativeQuery(SQL_TABLE).executeUpdate();
-            transaction.commit();
-        } catch (Exception e) {
-            if (null != transaction) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
+        execute(SQL_TABLE);
     }
 
     @Override
     public void dropUsersTable() {
-        Transaction transaction = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            NativeQuery nativeQuery = session.createNativeQuery(dropTable);
-            nativeQuery.executeUpdate();
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
+       execute(dropTable);
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
         Transaction transaction = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        Session session = Util.getSessionFactory().openSession();
+        try{
             transaction = session.beginTransaction();
-            session.merge(new User(name, lastName, age));
+            session.save(new User(name,lastName,age));
             transaction.commit();
         } catch (Exception e) {
             e.printStackTrace();
             if (transaction != null) {
                 transaction.rollback();
+                e.printStackTrace();
+            }
+        }  finally {
+            try{
+                session.close();
+            }catch (Exception e){
                 e.printStackTrace();
             }
         }
@@ -80,7 +70,8 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void removeUserById(long id) {
         Transaction transaction = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        Session session = Util.getSessionFactory().openSession();
+        try {
             transaction = session.beginTransaction();
             session.remove(session.get(User.class, id));
             transaction.commit();
@@ -89,34 +80,56 @@ public class UserDaoHibernateImpl implements UserDao {
             if (transaction != null) {
                 transaction.rollback();
             }
+        }finally {
+            try{
+                session.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
 
     @Override
     public List<User> getAllUsers() {
         List<User> users = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        Session session = Util.getSessionFactory().openSession();
+        try {
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery<User> criteria = builder.createQuery(User.class);
             criteria.from(User.class);
             users = session.createQuery(criteria).getResultList();
+        } finally {
+            try{
+                session.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
         return users;
     }
 
     @Override
     public void cleanUsersTable() {
+        execute(cleanTAble);
+    }
+    public void execute (String sqlCommand ) {
         Transaction transaction = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        Session session = Util.getSessionFactory().openSession();
+        try {
             transaction = session.beginTransaction();
-            NativeQuery nativeQuery = session.createSQLQuery(cleanTAble);
-            nativeQuery.executeUpdate();
+            session.createSQLQuery(sqlCommand).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
-            if (transaction != null) {
+            if (null != transaction) {
                 transaction.rollback();
             }
             e.printStackTrace();
+        } finally {
+            try{
+                session.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
 }
